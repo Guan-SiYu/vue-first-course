58a3dbe284fdf5daef30b000e33941d77a3924bb
guansiyu <guansiyu666@yeah.net>
2021-07-02 17:21:47 +0800

    no message

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>hiiii</h1>
+    <Tasks :tasks="tasks" />
+</template>
+<script>
+import Tasks from "../components/Tasks.vue";
+export default {
+    name: "Home",
+    components: {
+        Tasks,
+    },
+    data() {
+        return { tasks: [] };
+    },
+    props: [],
+    methods: {
+        async fetchTasks() {
+            const data = await fetch("http://localhost:5001/tasks");
+            return data.json();
+        },
+    },
+    async created() {
+        console.log("tasks rendering");
+        this.tasks = await this.fetchTasks();
+    },
+};
+</script>
